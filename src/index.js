938723a9d4758dfab7b4e0357188ce87f7754412
vvsar <vvsar@mail.ru>
2020-10-19 22:58:40 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let str = n.toString();
+  let arr = str.split("");
+  arr = arr.reverse();
+  str = arr.join("");
+  let rev = parseInt(str);
+  return rev;
 }
