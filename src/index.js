b4f36062ffca5cf205ee863ca53d377f8c2fca77
Victor Saraev <vvsar@mail.ru>
2021-02-16 02:20:04 +0300

    feat: add comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 module.exports = function reverse (n) {
+    // number to string
   let str = n.toString();
+    // string to array
   let arr = str.split("");
+    // reverse array
   arr = arr.reverse();
+    // array to string
   str = arr.join("");
+    // string to number
   let rev = parseInt(str);
   return rev;
 }
